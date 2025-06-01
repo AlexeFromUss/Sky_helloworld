6f8daa67aae6f0cc56b755d9cef16942df8c2202
Alex <ussuralexey@gmail.com>
2025-06-01 23:28:30 +1000

    HW-2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,70 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Привет, Мир!");
-        System.out.printf("Hello World!");
+        //Zadacha 1
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Собачек " + dog);
+        System.out.println("Кошечек " + cat);
+        System.out.println("Листочков " + paper);
+
+        //Zadacha 2
+        dog = dog + 4;
+        cat += 4;
+        paper += 4;
+
+        //Zadacha 3
+        dog = dog - 3.5;
+        cat -= 1.6;
+        paper -= 7639;
+
+        System.out.println("Псов " + dog);
+        System.out.println("\"Независымых\" " + cat);
+        System.out.println("Мукулатуры " + paper);
+
+        //Zadacha 4
+        var friend = 19;
+        System.out.println("Большая компания: " + friend + " друзей");
+        friend += 2;
+        System.out.println("Большая компания, стало: " + friend + " друзей");
+        friend /= 7;
+        System.out.println("Большая компания была: " + friend + " друзей");
+
+        //Zadacha 5
+        var frog = 3.5;
+        System.out.println("Лягушек: " + frog);
+        frog *= 10;
+        System.out.println("Лягушек: " + frog);
+        frog /= 3.5;
+        System.out.println("Лягушек: " + frog);
+        frog += 4;
+        System.out.println("Лягушек: " + frog);
+
+        //Zadacha 6
+        var fighterOne = 78.2;
+        var fighterTwo = 82.7;
+        System.out.println("Общая масса бойцов: " + (fighterOne + fighterTwo));
+        System.out.println("Разница в массе бойцов: " + (fighterOne - fighterTwo));
+
+        //Zadacha 7
+        var Ostatok = fighterTwo % fighterOne;
+        System.out.println("Остаток от деления масс бойцов: " + (Ostatok));
+
+        //Zadacha 8
+        var allTime = 640;
+        var workTime = 8;
+        var quantityHuman = allTime / workTime;
+        System.out.println("Всего работников в компании — " + (quantityHuman) + " человек");
+        quantityHuman += 94;
+        allTime = quantityHuman * workTime;
+        System.out.println("Если в компании работает " + (quantityHuman) + " человек, то всего " + allTime + " часов работы может быть поделено между сотрудниками");
+
+
+
+
+
     }
 }
