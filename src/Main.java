9d459ecbc3eb64b475ffe9b926d26a8593483492
Alex <ussuralexey@gmail.com>
2025-06-16 23:05:32 +1000

    HW-10 исправлено

--- src/Main.java
+++ src/Main.java
@@ -12,38 +12,53 @@ public class Main {
         short [] adress = {192, 168, 1, 1};
 
         //Zadacha 2
-        for (int i = 0; i < arrayOne.length-1; i++) {
-            System.out.print(arrayOne[i] + ", ");
+        for (int i = 0; i < arrayOne.length; i++) {
+            System.out.print(arrayOne[i] );
+            if (i < arrayOne.length-1) {
+                System.out.print(", ");
+            }
         }
-        System.out.println(arrayOne[2]);
-        for (int i = 0; i < arrayTwo.length-1; i++) {
-            System.out.print(arrayTwo[i] + ", ");
+        System.out.println();
+        for (int i = 0; i < arrayTwo.length; i++) {
+            System.out.print(arrayTwo[i]);
+            if (i < arrayTwo.length-1) {
+                System.out.print(", ");
+            }
         }
-        System.out.println(arrayTwo[2]);
-        //Т.к. в задаче не указано каким именно циклом пользоваться попробую while
-        int j = 0;
-        while (j < adress.length-1) {
-            System.out.print(adress[j] + ",");
-            j++;
+        System.out.println();
+
+        for (int i = 0; i < adress.length; i++) {
+            System.out.print(adress[i]);
+            if (i < adress.length-1) {
+                System.out.print(",");
+            }
         }
-        System.out.println(adress[j]);
+        System.out.println();
 
         //Zadacha 3
-        for (int i = arrayOne.length-1; i > 0; i--) {
-            System.out.print(arrayOne[i] + ", ");
+        for (int i = arrayOne.length-1; i >= 0; i--) {
+            System.out.print(arrayOne[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
         }
-        System.out.println(arrayOne[0]);
-        for (int i = arrayTwo.length-1; i > 0; i--) {
-            System.out.print(arrayTwo[i] + ", ");
+        System.out.println();
+
+        for (int i = arrayTwo.length-1; i >= 0; i--) {
+            System.out.print(arrayTwo[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
         }
-        System.out.println(arrayTwo[0]);
-        //Т.к. в задаче не указано каким именно циклом пользоваться попробую while
-        j = adress.length-1;
-        while (j > 0) {
-            System.out.print(adress[j] + ",");
-            j--;
+        System.out.println();
+
+        for (int i = adress.length-1; i >= 0; i--) {
+            System.out.print(adress[i]);
+            if (i > 0) {
+                System.out.print(",");
+            }
         }
-        System.out.println(adress[j]);
+        System.out.println();
 
         //Zadacha 4
         for (int i = 0; i < arrayOne.length; i++) {
